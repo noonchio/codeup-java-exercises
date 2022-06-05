36ef9d8f1576780428bf9c4ee64e5fcecb8e2a38
Alfredo Oviedo <alfredo.g.oviedo08@gmail.com>
2022-06-04 19:48:51 -0500

    add new code.java

---
+++ src/Code.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Code {
+    public static void main(String[] args) {
+/*
+ Swap two variables
+      String x = "water";
+      String y = "Kool-Aid";
+      String temp;
+
+      temp= x;
+      x=y;
+      y=temp;
+        System.out.println("x: "+x);
+        System.out.println("y: "+y);
+*/
+
+/*
+  User Input
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("What is your favorite food?");
+        String food = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+        System.out.println("You are "+age+" years old");
+        System.out.println("You like " + food);
+*/
+        /*
+         expression = operands & operators
+         operands = values, variables, numbers, quantity
+         operators = + - * / %
+                int friends = 10;
+
+                friends--;
+
+                System.out.println(friends);
+        */
+/*
+ Graphical User Interface
+        String name = JOptionPane.showInputDialog("Enter Your name");
+        JOptionPane.showMessageDialog(null, "Hello "+name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "You are "+age+" years old");
+*/
+
+
+
+    }
+}
