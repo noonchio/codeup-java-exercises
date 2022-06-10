522afab67143679506d864fb76763ebc4f9679b9
Alfredo Oviedo <alfredo.g.oviedo08@gmail.com>
2022-06-10 16:13:48 -0500

    add Person.jave

---
+++ src/DishTest.java
@@ -0,0 +1,14 @@
+public class DishTest {
+
+    public static void main(String[] args) {
+
+        Dish dish1 = new Dish();
+
+        dish1.nameOfDish = "Sushi Roll";
+        dish1.costInCents = 1000;
+         dish1.wouldRecommend = true;
+
+         dish1.printSummary();
+
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+    private String name;
+
+
+    public String getName(){
+//TODO: return the person's name
+        return name;
+    }
+
+    public void setName(String name){
+//TODO: change the name field to the passed value
+    this.name = name;
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+        System.out.println("Hello " + name);
+    }
+
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.setName("Alfredo");
+        person1.sayHello();
+    }
+}
